7440442229561fba20a6108887866716472d2b3e
leo <krokxa228@gmail.com>
2024-08-23 16:44:24 +0500

    code_factoring

--- async_request.py
+++ async_request.py
@@ -6,68 +6,36 @@ from models import init_orm, Session, SwapiPeople
 
 MAX_REQUESTS = 30
 
-async def get_people(session, person_id):
-    response = await session.get(f'https://swapi.py4e.com/api/people/{person_id}/')
+async def fetch_data(session, endpoint):
+    response = await session.get(endpoint)
     json_data = await response.json()
     return json_data
 
-async def get_film_title(session, film_id):
-    response = await session.get(f'https://swapi.py4e.com/api/films/{film_id}/')
-    json_data = await response.json()
-    return json_data['title']
+async def get_resource_name(session, resource_type, resource_id):
+    endpoint = f'https://swapi.py4e.com/api/{resource_type}/{resource_id}/'
+    json_data = await fetch_data(session, endpoint)
+    return json_data.get('name') or json_data.get('title')
 
-async def get_species_name(session, species_id):
-    response = await session.get(f'https://swapi.py4e.com/api/species/{species_id}/')
-    json_data = await response.json()
-    return json_data['name']
-
-async def get_starship_name(session, starship_id):
-    response = await session.get(f'https://swapi.py4e.com/api/starships/{starship_id}/')
-    json_data = await response.json()
-    return json_data['name']
-
-async def get_vehicle_name(session, vehicle_id):
-    response = await session.get(f'https://swapi.py4e.com/api/vehicles/{vehicle_id}/')
-    json_data = await response.json()
-    return json_data['name']
-
-async def get_homeworld_name(session, homeworld_id):
-    response = await session.get(f'https://swapi.py4e.com/api/planets/{homeworld_id}/')
-    json_data = await response.json()
-    return json_data['name']
+async def fetch_and_join_names(session, resource_type, resource_urls):
+    resource_ids = [url.split('/')[-2] for url in resource_urls]
+    print(resource_ids)
+    name_tasks = [get_resource_name(session, resource_type, resource_id) for resource_id in resource_ids]
+    names_list = await asyncio.gather(*name_tasks)
+    return ', '.join(names_list)
 
 async def insert_json_list(web_session, json_list):
     async with Session() as db_session:
         for item in json_list:
             if 'detail' not in item:
                 try:
-                    # Process films
-                    film_ids = [film.split('/')[-2] for film in item.get('films', [])]
-                    film_title_tasks = [get_film_title(web_session, film_id) for film_id in film_ids]
-                    film_titles_list = await asyncio.gather(*film_title_tasks)
-                    films = ', '.join(film_titles_list)
-
-                    # Process species
-                    species_ids = [species.split('/')[-2] for species in item.get('species', [])]
-                    species_name_tasks = [get_species_name(web_session, species_id) for species_id in species_ids]
-                    species_names_list = await asyncio.gather(*species_name_tasks)
-                    species = ', '.join(species_names_list)
-
-                    # Process starships
-                    starship_ids = [starship.split('/')[-2] for starship in item.get('starships', [])]
-                    starship_name_tasks = [get_starship_name(web_session, starship_id) for starship_id in starship_ids]
-                    starship_names_list = await asyncio.gather(*starship_name_tasks)
-                    starships = ', '.join(starship_names_list)
-
-                    # Process vehicles
-                    vehicle_ids = [vehicle.split('/')[-2] for vehicle in item.get('vehicles', [])]
-                    vehicle_name_tasks = [get_vehicle_name(web_session, vehicle_id) for vehicle_id in vehicle_ids]
-                    vehicle_names_list = await asyncio.gather(*vehicle_name_tasks)
-                    vehicles = ', '.join(vehicle_names_list)
+                    films = await fetch_and_join_names(web_session, 'films', item.get('films', []))
+                    species = await fetch_and_join_names(web_session, 'species', item.get('species', []))
+                    starships = await fetch_and_join_names(web_session, 'starships', item.get('starships', []))
+                    vehicles = await fetch_and_join_names(web_session, 'vehicles', item.get('vehicles', []))
 
                     # Process homeworld
                     homeworld_id = item.get('homeworld').split('/')[-2] if item.get('homeworld') else None
-                    homeworld_name = await get_homeworld_name(web_session, homeworld_id) if homeworld_id else 'unknown'
+                    homeworld_name = await get_resource_name(web_session, 'planets', homeworld_id) if homeworld_id else 'unknown'
 
                     orm_model = SwapiPeople(
                         id=item.get('id'),
@@ -94,7 +62,7 @@ async def main():
     await init_orm()
     async with aiohttp.ClientSession() as web_session:
         for people_ids in chunked(range(1, 101), MAX_REQUESTS):
-            coros = [get_people(web_session, i) for i in people_ids]
+            coros = [fetch_data(web_session, f'https://swapi.py4e.com/api/people/{i}/') for i in people_ids]
             json_list = await asyncio.gather(*coros)
             await insert_json_list(web_session, json_list)
 
